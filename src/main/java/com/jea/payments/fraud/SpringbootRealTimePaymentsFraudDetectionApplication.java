63bda5642dc468bc9e7e2c5897cc322ccf52dcbc
Jagadeesh <blockchian@jeadigital.com>
2024-11-14 21:06:05 +0530

    Initial commit

---
+++ src/main/java/com/jea/payments/fraud/SpringbootRealTimePaymentsFraudDetectionApplication.java
@@ -0,0 +1,13 @@
+package com.jea.payments.fraud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRealTimePaymentsFraudDetectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRealTimePaymentsFraudDetectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jea/payments/fraud/SpringbootRealTimePaymentsFraudDetectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jea.payments.fraud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRealTimePaymentsFraudDetectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
